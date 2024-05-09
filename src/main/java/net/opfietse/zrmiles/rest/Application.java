98311392591f0a6e08e0829761462ca9afead775
Mark Reuvekamp <kawazr7@xs4all.nl>
2024-05-09 10:48:19 +0200

    Base page with CSS and menu

--- src/main/java/net/opfietse/zrmiles/rest/Application.java
+++ src/main/java/net/opfietse/zrmiles/rest/Application.java
@@ -5,16 +5,24 @@ import jakarta.ws.rs.Path;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 import io.quarkiverse.renarde.Controller;
+import jakarta.ws.rs.Produces;
 
 public class Application extends Controller {
 
     @CheckedTemplate
     static class Templates {
         public static native TemplateInstance index();
+        public static native TemplateInstance zrmilescss();
     }
 
     @Path("/")
     public TemplateInstance index() {
         return Templates.index();
+    }
+
+    @Path("/zrmiles.css")
+    @Produces("text/css")
+    public TemplateInstance css() {
+        return Templates.zrmilescss();
     }
 }
