c192f56e1752834710df52e4889691945ee1e502
Mark Reuvekamp <kawazr7@xs4all.nl>
2024-05-09 11:04:49 +0200

    Show app version

--- src/main/java/net/opfietse/zrmiles/rest/Application.java
+++ src/main/java/net/opfietse/zrmiles/rest/Application.java
@@ -6,18 +6,21 @@ import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 import io.quarkiverse.renarde.Controller;
 import jakarta.ws.rs.Produces;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 public class Application extends Controller {
+    @ConfigProperty(name = "quarkus.application.version")
+    java.util.Optional<String> version;
 
     @CheckedTemplate
     static class Templates {
-        public static native TemplateInstance index();
+        public static native TemplateInstance index(String version);
         public static native TemplateInstance zrmilescss();
     }
 
     @Path("/")
     public TemplateInstance index() {
-        return Templates.index();
+        return Templates.index(version.orElse("Beta"));
     }
 
     @Path("/zrmiles.css")
