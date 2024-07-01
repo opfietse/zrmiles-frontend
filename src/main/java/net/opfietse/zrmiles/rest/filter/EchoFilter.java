688eeb66492da08eb41eab8cfde2cc40cf93cdf7
Mark Reuvekamp <kawazr7@xs4all.nl>
2024-07-01 08:43:36 +0200

    Print headers

---
+++ src/main/java/net/opfietse/zrmiles/rest/filter/EchoFilter.java
@@ -0,0 +1,15 @@
+package net.opfietse.zrmiles.rest.filter;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class EchoFilter implements ContainerRequestFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        requestContext.getHeaders().keySet().stream().forEach(h -> System.out.println(h + ": " + requestContext.getHeaders().get(h)));
+    }
+}
