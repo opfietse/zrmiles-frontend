c61530bbf24ca53367ac6c4682fe0584b0a9986a
Mark Reuvekamp <kawazr7@xs4all.nl>
2024-08-09 20:01:59 +0200

    Add date page

---
+++ src/main/java/net/opfietse/zrmiles/date/reports/DateResource.java
@@ -0,0 +1,32 @@
+package net.opfietse.zrmiles.date.reports;
+
+import io.quarkiverse.renarde.Controller;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateResource extends Controller {
+    @CheckedTemplate
+    static class Templates {
+
+        static native TemplateInstance date(String dateAssen, String dateLondon, String dateUtc);
+    }
+
+    @GET
+    @Path("/date")
+    public TemplateInstance date() {
+        ZonedDateTime now = ZonedDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        return Templates.date(
+            formatter.format(now),
+            formatter.format(now.withZoneSameInstant(ZoneId.of("Europe/London"))),
+            formatter.format(now.withZoneSameInstant(ZoneId.of("UTC")))
+        );
+    }
+}
